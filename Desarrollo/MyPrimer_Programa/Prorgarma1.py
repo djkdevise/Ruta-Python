d5db7dd3ef46ccaf85a3fcaf5c47fa5337552597
Jamith Garcia <jamith.garcia.ext@inetum.com>
2023-11-20 17:17:29 -0500

    first commit

---
+++ Desarrollo/MyPrimer_Programa/Prorgarma1.py
@@ -0,0 +1,44 @@
+print('Hisssss')
+
+# Código con error de sintaxis
+print("Hola Mundo")
+# Corrección
+print("Hola Mundo")
+
+# Código con error de nombre de variable
+mensaje = "Hola Mundo"
+#print(mesaje)
+# Corrección
+print(mensaje)
+
+# Código con error de tipo de datos
+numero = "10"
+suma = numero + 5
+# Corrección
+suma = int(numero) + 5
+
+# Código con error de tipo de datos
+numero = "10"
+suma = numero + 5
+# Corrección
+suma = int(numero) + 5
+
+# Código con error de índice en listas
+lista = [1, 2, 3]
+elemento = lista[3]
+# Corrección
+elemento = lista[2]
+
+# Código con error lógico
+edad = 15
+if edad > 18 and edad < 21:
+    print("Eres mayor de 18 pero menor de 21")
+# Corrección
+if 18 < edad < 21:
+    print("Eres mayor de 18 pero menor de 21")
+
+      
+# Código con error de importación
+import matplotib.pyplot as plt
+# Corrección
+import matplotlib.pyplot as plt
