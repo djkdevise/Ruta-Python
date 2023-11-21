6a45617df4d96e623b3550332436fa64a6d6f3f0
Jamith Garcia <jamith.garcia.ext@inetum.com>
2023-11-20 21:30:17 -0500

    Trabajando con Literales en Python

---
+++ Desarrollo/MyPrimer_Programa/Prorgarma3_Numeros_enteros.py
@@ -0,0 +1,28 @@
+
+#Creacion de literales
+#Enteros
+##pirnt(123)
+
+#Flotantes
+##print(0o123)
+##print(0x123)
+
+
+#flotanes
+
+##print(.4)
+
+#Notacion cientifica usando la letra (e)
+# la Velocidad de la luz 300000000 mts x seg. se puede escribir 3 x 10 a la 8
+#Python:
+##print(3E8)
+
+#Codificando flotantes ej: La Constante de Planck (denotada como h 6.62607 x 10 a la -34.)
+#Python:
+##print(6.62607E-34)
+
+print(0.0000000000000000000001)
+
+
+
+
