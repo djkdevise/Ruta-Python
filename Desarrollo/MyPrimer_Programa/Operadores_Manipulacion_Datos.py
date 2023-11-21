5d5b4e610734ed59cec86229cd92b4a35d339c8b
Jamith Garcia <jamith.garcia.ext@inetum.com>
2023-11-20 23:27:06 -0500

    Operadores aritmeticos en Python

---
+++ Desarrollo/MyPrimer_Programa/Operadores_Manipulacion_Datos.py
@@ -0,0 +1,38 @@
+#2.3 Seccion Operadores y herramientas de manipulacion de datos.
+#Suma:
+#print(2+2)
+#+ = Suma
+#- = Sustraccion
+#* = Multiplicacion
+#/ = Division
+#//= división entera ( el resultado carece dela parte fraccionaria o es redondeado)
+#% = módulo o residuo
+#** = Esponetes o potenciacion
+
+#print(-6/4)
+#print(6./-4)
+
+#Modulo:
+
+#print(14%4)
+#print(12 // 4.5)
+#print(-3 ** 2)
+#print(-2 ** 3)
+#print(-(3 ** 2))
+#print(2 * 3 % 5)
+#print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+#3print(4 ** -1)
+#print((2 ** 4), (2 * 4.), (2 * 4))
+#print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+
+
+
+
+
+
+
+
+    
+    
+
